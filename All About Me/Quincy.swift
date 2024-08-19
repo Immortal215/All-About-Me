42e080447e498a34699cbc0ff0cf0699a398d072
Immortal215 <sharulshah@icloud.com>
2024-08-19 08:59:51 -0500

    fixes

--- All About Me/Quincy.swift
+++ All About Me/Quincy.swift
@@ -1,8 +1,1 @@
-//
-//  Quincy.app.swift
-//  All About Me
-//
-//  Created by Sharul M. Shah on 8/19/24.
-//
-
-import Foundation
+import SwiftUI
--- All About Me/Sharul.swift
+++ All About Me/Sharul.swift
@@ -1,8 +1,1 @@
-//
-//  Sharul.swift
-//  All About Me
-//
-//  Created by Sharul M. Shah on 8/19/24.
-//
-
-import Foundation
+import SwiftUI
