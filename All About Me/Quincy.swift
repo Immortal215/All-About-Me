5aa648220f9f810193dc7441ddd0d41fe8f504d6
Ramen-4life <quincyalex09@gmail.com>
2024-08-19 09:25:28 -0500

    Did my side

--- All About Me/Quincy.swift
+++ All About Me/Quincy.swift
@@ -1,15 +1,41 @@
 import SwiftUI
 
 struct Quincy: View {
-    var color = "blue"
+    @State var year = 10
+    @State var textYear = "Sophmore"
+    @State var events = ""
     
     var body: some View {
-        Button(action: {
-            
-        }, label: {
+        VStack {
             Text("Quincy Alex")
-                .foregroundColor(Color(color))
-        })
+                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+            Button(action: {
+                year += 1
+                if year > 12 {
+                    year = 10
+                    textYear = "Sophmore"
+                }
+                if year == 10 {
+                    textYear = "Sophmore"
+                } else if year == 11 {
+                    textYear = "Junior"
+                } else if year == 12 {
+                    textYear = "Senior"
+                }
+            }, label: {
+                Text("\(year)th grade\n\(textYear) Year")
+                    .foregroundColor(.red)
+            })
+            Text("I run track")
+            Button(action: {
+                events += "100m, 200m"
+                if events == "100m, 200m100m, 200m" {
+                    events = ""
+                }
+            }, label: {
+                Text("Events: \(events)")
+            })
+        }
         
     }
 }
